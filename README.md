0469d83e36bb46ddeccf518a8f381ac9cd19f1b5
MyeongHyeonYoo <victorea5@naver.com>
2023-01-22 13:49:58 +0900

    Multi Thread   main Thread + Working Thread

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 ---
 
 
-#### ◾ [Single Thread]
+#### ◾ [Single Thread] - main Thread
 
 <img src="img/sound_and_print.jpg">
 
@@ -25,13 +25,25 @@
 
 ---
 
+#### ◾ [Multi Thread] - main Thread & Working Thread
 
+- ##### Thread 클래스 사용 - 객체 생성
+- ##### run() 메소드 재정의
+- ##### thread.start() 메소드 호출하여 run() 실행
+
+<img src="img/sound_and_print.jpg">
+
+##### 소리가 나면서 문자가 출력되는 동시적 작업이 진행이 되는 것을 확인.
 
 ```
 rest time :D
 
 Thread 클래스 사용 (Thread.sleep() 메소드 호출 - 지연)
 Toolkit 클래스 사용 (toolkit.beep() 메소드 호출 - 소리)
+
+main thread가 실행 중, 
+working thread(thread.start() -> run()) 실행되면서
+동시 다발적으로 작업이 진행되는 것을 확인할 수 있다.
 
 ```
 
--- src/MultiThread/Toolkit_Thread.java
+++ src/MultiThread/Toolkit_Thread.java
@@ -6,19 +6,29 @@ public class Toolkit_Thread {
 
 	public static void main(String[] args) {
 		
-		// 작업 1 (소리 | 소리 안 들릴 시 이어폰 사용하여 확인)
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		for (int i = 0; i < 5; i++) {
-			toolkit.beep();
-			try { Thread.sleep(500); } catch (Exception e) {}
-		}
+		// Thread 클래스를 생성하고,
+		// Thread 클래스 안에 run() 메소드를 재정의.
+
+		// main 스레드와 거의 동시 작업
+		Thread thread = new Thread(new Runnable() {
+			
+				@Override
+				public void run() {
+				// 작업 1 (소리 | 소리 안 들릴 시 이어폰 사용하여 확인)
+				Toolkit toolkit = Toolkit.getDefaultToolkit(); // 정적 메소드로 Toolkit 객체 얻기
+				for (int i = 0; i < 5; i++) {
+					toolkit.beep(); // 소리 출력
+					try { Thread.sleep(500); } catch (Exception e) {} // 지연 효과 -> try ~ catch로 사용하여야 컴파일 된다.
+				}
+			}
+		});
+		
+		thread.start();
 		
 		// 작업 2 (출력)
 		for (int i = 0; i < 5; i++) {
-			System.out.println("띵");
-			try { Thread.sleep(500); } catch (Exception e) {}
+			System.out.println("띵"); // 문자 출력
+			try { Thread.sleep(500); } catch (Exception e) {} // 지연 효과 -> try ~ catch로 사용하여야 컴파일 된다.
 		}
-
 	}
-
 }
